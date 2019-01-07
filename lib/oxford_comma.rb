976e8d04af713c64abaa425507bc8d4374fa4432
Aaron Berns <anberns@gmail.com>
2019-01-07 20:09:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,7 @@ def oxford_comma(array)
   arr = array
   last = arr.last
   arr.pop
-  
-  array.join(",")
+  str = arr.join(",") + " and #{last}"
+  str
 end
 
-Write a method `oxford_comma` that takes an argument array of string elements and converts it into a string using the Oxford comma. For example, the array `["fiddleheads","okra","kohlrabi"]` should get converted to the string `"fiddleheads, okra, and kohlrabi"`.
